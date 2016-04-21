8bec590d750340ade7d9c79317bde50323f8fba6
swallow <swallow@taomee-ex.com>
2016-04-21 23:43:23 +0800

    add

---
+++ CsLei/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsLei
+{
+    public class Class2 : MainWindow
+    {
+        public void getTime()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
